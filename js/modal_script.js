39fff6af899208ac1db68dd8527366ffb5f09a6e
VlaSovaAnna <anja_kaa@mail.ru>
2017-05-10 10:26:17 +0300

    JS
    Добавлена интерактивная карта и анимация формы, а такж выпадающее меню.

--- js/modal_script.js
+++ js/modal_script.js
@@ -1,7 +1,10 @@
 {  var link = document.querySelector(".btn-feedback-form");
   var popup = document.querySelector(".modal-content");
+  var linkMap = document.querySelector(".map");
+  var popupMap = document.querySelector(".modal-map");
   var overlay = document.querySelector(".overlay");
   var close = popup.querySelector(".modal-content-close");
+  var closeMap = popupMap.querySelector(".modal-map-close");
   var form = popup.querySelector("form");
   var ffname = popup.querySelector("[name=feedback-form-name]");
   var ffemail = popup.querySelector("[name=feedback-form-email]");
@@ -14,11 +17,24 @@
     ffname.focus();
   });
 
+  linkMap.addEventListener("click", function(event) {
+    event.preventDefault();
+    popupMap.classList.add("modal-content-show");
+    overlay.classList.add("overlay-show");
+  });
+
   close.addEventListener("click", function(event) {
     event.preventDefault();
     popup.classList.remove("modal-content-show");
     overlay.classList.remove("overlay-show");
   });
+
+  closeMap.addEventListener("click", function(event) {
+     event.preventDefault();
+     popupMap.classList.remove("modal-content-show");
+     overlay.classList.remove("overlay-show");
+   });
+
 
   form.addEventListener("submit", function(event) {
     if (!ffname.value || !ffemail.value || !ffcontent.value) {
@@ -31,6 +47,10 @@
     if (event.keyCode === 27) {
       if (popup.classList.contains("modal-content-show")) {
         popup.classList.remove("modal-content-show");
+        overlay.classList.remove("overlay-show");
+      }
+      if (popupMap.classList.contains("modal-content-show")) {
+        popupMap.classList.remove("modal-content-show");
         overlay.classList.remove("overlay-show");
       }
     }
